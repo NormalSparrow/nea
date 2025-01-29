7ab6a92e35d335ca1d2616e44ee66612a7734521
Kaden Sparrow <kaden.sparrow@gmail.com>
2025-01-29 13:46:50 +0000

    Text change

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,19 @@ function App() {
   
   return (
     <div class="p-4 h-screen bg-slate-900 flex flex-col justify-center items-center space-y-2">
-      <p class = "text-sky-50">the mewo mewo mSeowmeow  </p>
+      <p class = "text-sky-50">Synthesizer </p>
       <div class="space-x-2">
-      <button class= " h-7 w-7 flex-auto rounded-full p-1 bg-slate-300 hover:bg-white transition">
+      <button class= " h-7 w-7 flex-auto rounded-full p-1 bg-slate-300 hover:bg-white transition"> {/* sawtoothWave button */}
           <img src="/sawtooth-wave.svg" alt="sawtooth"/>
         </button>
-        <button class= " h-7 w-7 flex-auto rounded-full p-1 bg-slate-300 hover:bg-white transition">
+        <button class= " h-7 w-7 flex-auto rounded-full p-1 bg-slate-300 hover:bg-white transition">{/* TriangleWave button */}
          <img src="/triangle-wave-icon-size_64.png" alt = "triangle"/>
         </button>
-        <button class= " h-7 w-7 flex-auto rounded-full p-1 bg-slate-300 hover:bg-white transition">
+        <button class= " h-7 w-7 flex-auto rounded-full p-1 bg-slate-300 hover:bg-white transition">{/* SquareWave button */}
           <img src= "/square-wave-icon-size_64.png" alt = "square"/>
           </button>
           <button class= " h-7 w-7 flex-auto rounded-full p-1 bg-slate-300 hover:bg-white transition" onClick={karplus}>
-          <img src= "/sine-wave-icon-size_64.png" alt = "karplus" />
+          <img src= "/sine-wave-icon-size_64.png" alt = "karplus" /> {/* StringDecay button */} 
 
           </button>
       </div>
@@ -54,34 +54,34 @@ function karplus() {
   var sampleRate = 48000;
   let samples = sampleRate * seconds;
   var size= int16Buffer.length*2;
-  var fs = require("fs")//d
+  var fs = require("fs")//loads fs module for files
   var file = fs.createWriteStream("karplus.wav")//makes a stream to pour data nto file
-  var buf = [];
-  var buffer = Buffer.allocUnsafe(size);
+  var buf = [];//array for values
+  var buffer = Buffer.allocUnsafe(size);//to hold the audio data when converted to binary
 
   for (let n = 0; n < sampleRate / 55; n++) {
       buf.push(Math.random() * 2.0 - 1.0);
   }
   
   var finalBuffer = [];
-  for (let n = 0; n < samples; n++) {
+  for (let n = 0; n < samples; n++) { //for each value of sample
       let first = buf.shift();
       let next = buf[0];
-      let sample = 0.996 * 0.5 * (first + next);
+      let sample = 0.996 * 0.5 * (first + next);// karplus algorithm to simulate decay
       buf.push(sample);
       finalBuffer.push(sample);
   }
   
   var int16Buffer = Int16Array.from(
-     finalBuffer.map((n) => n * 32767) //convert to 16-bit range, 1 = 32767
+     finalBuffer.map((n) => n * 32767) //convert to 16bit range, 1 = 32767 
   );  
   
-  file.write(buffer.alloc(44));
-  int16Buffer.forEach((value, index) => {
+  file.write(buffer.alloc(44)); //lets use the 44 bytes in the header constructed
+  int16Buffer.forEach((value, index) => { //a loop to write each buffer value into the file
       buffer.writeInt16LE(value, index * 2);
   });
   
-  file.write(buffer);
+  file.write(buffer);// file write
   file.end();
 }
 
